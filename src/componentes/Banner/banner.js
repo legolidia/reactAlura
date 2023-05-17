75768a65e5fa84828f907ccdc84aa62698b01e18
legolidia <lidiacarolinaandrade@gmail.com>
2023-05-17 15:50:11 -0300

    Curso React Alura

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+function Banner() {
+    return(
+        <header className="banner">
+        <img src="/img/banner.png" alt="banner principal da página do Organo" className="banner" width="100%"></img>
+        </header>
+        )
+}
+
+export default Banner
