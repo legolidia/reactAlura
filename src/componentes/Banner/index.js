4ff9e34ae810806548956a97e6253a7a5e666ef0
legolidia <lidiacarolinaandrade@gmail.com>
2023-05-22 15:21:04 -0300

    curso react finalizado

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -2,6 +2,7 @@ import './banner.css'
 
 const Banner = () => {
     return(
+        //<Fragment> || <> </>
         <img src="/img/banner.jpeg" alt="banner principal da página do Organo" className="banner" width="100%"></img>
     )
 }
---
+++ src/componentes/pokemons.js
@@ -0,0 +1,20 @@
+import { waitForElementToBeRemoved } from "@testing-library/react"
+
+grass
+water 
+fire
+poison
+fairy
+dark
+electric
+bug
+flying
+fighting
+normal
+ground
+rock
+ghost
+psychic
+ice
+dragon
+steel
